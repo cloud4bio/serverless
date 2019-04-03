a091ce1349af648b2ffa2554567f2f2dd779991e
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-03 11:57:40 -0400

    server working?

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ console.log('server started at '+Date())
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -11,7 +11,5 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+server.listen(port);
 
