a7db27c0694b5fabf33183ddb25c9d86b87b5df4
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-03 11:13:11 -0400

    loading pacreatic cancer

--- jonas/jonas.js
+++ jonas/jonas.js
@@ -3,10 +3,17 @@ console.log('jonas.js loaded')
 jonas=function(){
     // ini
     console.log(`ini at ${Date()}`)
+    let div=document.createElement('div')
+    document.body.appendChild(div)
+    jonas.div=div
+    fetch('https://health.data.ny.gov/resource/82xm-y6g8.json?hospital_county=Manhattan&ccs_diagnosis_description=Cancer%20of%20pancreas&facility_name=Memorial%20Hospital%20for%20Cancer%20and%20Allied%20Diseases'
+    ).then(x=>x.json().then(y=>{
+        jonas.data=y
+        jonas.div.innerHTML='<textarea id="txtData"></textarea>'
+        txtData.value=JSON.stringify(y,null,3)
+   }))
+
 }
 
 
 window.onload=jonas
-
-
-//fetch('https://health.data.ny.gov/resource/82xm-y6g8.json?hospital_county=Manhattan&ccs_diagnosis_description=Cancer%20of%20pancreas&facility_name=Memorial%20Hospital%20for%20Cancer%20and%20Allied%20Diseases').then(x=>x.json().then(console.log))
